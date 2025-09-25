140b6df683ae539dd5d995ff53f37a6397358da5
NahimMora <nahimsalta@gmail.com>
2025-09-24 23:22:12 -0300

    Caso pruebas Biseccion y Regul Falsi

--- Programacion_Numerica/EcuacionesNoLineales.py
+++ Programacion_Numerica/EcuacionesNoLineales.py
@@ -1,14 +1,16 @@
+import math
 import sympy as sp
 
-def biseccion(f, a, b, e):
+def biseccion(f, a, b, e= 1e-6):
     
     if f(a)*f(b) > 0:
         return None
     
     c = (a+b)/2
-    
+    i = 0
     while abs(f(c)) > e:
-        
+        i+=1
+        print(i, " a: ", a, " b: ", b, " c: ", c, " f(a): ", f(a), " f(c): ", f(c))
         if f(a)*f(c) < 0:
             b = c
         else:
@@ -17,14 +19,18 @@ def biseccion(f, a, b, e):
         c = (a+b)/2
     return c
 
-def regula_falsi(f, a, b, e):
+def regula_falsi(f, a, b, e=0.000001):
     
     if f(a)*f(b) > 0:
         return None
     
     c = (a*f(b) - b*f(a))/(f(b)-f(a))
+    i=0
     
     while abs(f(c)) > e:
+        
+        i+=1
+        print(i, " a: ", a, " b: ", b, " c: ", c, " f(a): ", f(a), " f(c): ", f(c))
         
         if f(a)*f(c) < 0:
             b = c
@@ -79,6 +85,18 @@ def metodo_newton(f, x1, e=1e-6):
     
     return x1
 
-f = lambda x: x**3 - x - 2
-raiz = metodo_newton(f, 1.5)
+def metodo_secante(f, x0, x1, e=1e-6, max_iter=100):
+    for _ in range(max_iter):
+        x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+        
+        if abs(x2 - x1) < e:
+            return x2 
+        
+        x0, x1 = x1, x2
+    
+    raise ValueError("No convergió en el número máximo de iteraciones")
+
+
+f = lambda x: math.e**x - x**2 + 1
+raiz = regula_falsi(f, -2, 0)
 print("Raíz aproximada:", raiz)
