ab06cee78c828230361c0792f1b1e5f6bd617de8
NahimMora <nahimsalta@gmail.com>
2025-10-02 12:18:10 -0300

    Metodos de raices de polinomios aplicados: determinar metodos de cotas: Lagrange, Laguerre, Newton

--- Programacion_Numerica/raicesPolinomios.py
+++ Programacion_Numerica/raicesPolinomios.py
@@ -1,28 +1,13 @@
 import math
 
-"""
-— Realizar un programa que incorpore procedimientos para:
-a) Calcular el valor de un polinomio para un determinado punto.
-b) Dividir un polinomio de grado n por otro de la forma a x  b.
-c) Dividir un polinomio de grado n por otro de la forma x2 + p x + q
-d) Determine las posibles raíces enteras.
-e) Determine las posibles raíces racionales.
-f) Determine las cotas de las raíces positivas y negativas por distintos métodos.
-g) Encontrar raíces reales por el método de Newtos para polinomios.
-— Implemente el Método de Bairstow.
-— Diseñe casos de pruebas y realice un informe sobre el funcionamiento de los programas.
+#a) Calcular el valor de un polinomio para un determinado punto.
+def evaluar_polinomio(P, x):
+    resultado = 0
+    for i in range(len(P)):
+        resultado += P[i] * (x ** i)
+    return resultado
 
-"""
-
-def calcularValor(P, x):
-    suma = 0
-    n = len(P)
-    
-    for i in range(1, n):
-        suma += (x**i)*P[i]
-        
-    return suma
-
+#b) Dividir un polinomio de grado n por otro de la forma a x  b.
 def ruffiniSimple(P, r):
     
     n = len(P)
@@ -36,6 +21,7 @@ def ruffiniSimple(P, r):
     
     return cociente, resto
 
+#c) Dividir un polinomio de grado n por otro de la forma x2 + p x + q
 def ruffiniDoble(P, Q):
     
     n = len(P)
@@ -55,6 +41,7 @@ def ruffiniDoble(P, Q):
     
     return cociente, resto
 
+#d) Determine las posibles raíces enteras.
 def encontrarDivisores(x):
     
     divisores = []
@@ -75,6 +62,7 @@ def posiblesRaicesEnteras(P):
     
     return posibles
 
+#e) Determine las posibles raíces racionales.
 def posiblesRaicesRacionales(P):
     
     n = len(P)
@@ -93,19 +81,128 @@ def posiblesRaicesRacionales(P):
     
     return posibles
 
-def datosLagrange(P):
+#f) Determine las cotas de las raíces positivas y negativas por distintos métodos.
+def coeficiente_negativo_max(P):
     
     n = len(P)
     
-    k = 0
+    k = -1
+    A = 0
+    
+    for i in range(1, n):
+        if  P[i] < 0:
+            if k == -1: k = i
+            
+            if A < abs(P[i]): A = abs(P[i])
+    return k, A
+
+def cota_superior(P):
+    n = len(P)
+    
+    a0 = P[n-1]
+    
+    k, A = coeficiente_negativo_max(P)
+    
+    if k == 0: return 1 + A/a0
+    
+    return 1 + (A/a0)**(1/k)
+
+def transformar_para_cotas_negativas(P):
+    
+    n = len(P)
+    P1 = []
     
     for i in range(n):
-        return 
+        if i % 2 != 0:
+            P1.append(-P[i])
+        else:
+            P1.append(P[i])
+
+    return P1
 
 def cotasLagrange(P):
     
-    cotas = []
+    c1 = cota_superior(P)
+    c2 = 1 / cota_superior(list(reversed(P)))
+    c4 = - cota_superior(transformar_para_cotas_negativas(P))
+    c3 = - 1 / cota_superior(list(reversed(transformar_para_cotas_negativas(P))))
     
+    return c4, c3, c2, c1
+
+def es_polinomio_positivo(Q):
     
-    for i in range(4):
-        return 
+    n = len(Q)
+    
+    esPositivo = True
+    
+    for i in range(n):
+        if Q[i] < 0: esPositivo = False
+        
+    return esPositivo
+        
+def evaluar_valores_cociente(P, limite = 100):
+    
+    num = 0
+    
+    while True and num < limite:
+        
+        num += 1    
+
+        Q, R = ruffiniSimple(P, num)
+        
+        if es_polinomio_positivo(Q):
+            break
+    
+    return num
+
+def cotasLaguerre(P):
+    
+    c1 = evaluar_valores_cociente(P)
+    c2 = 1 / evaluar_valores_cociente(list(reversed(P)))
+    c4 = - evaluar_valores_cociente(transformar_para_cotas_negativas(P))
+    c3 = - 1 / evaluar_valores_cociente(list(reversed(transformar_para_cotas_negativas(P))))
+    
+    return c4, c3, c2, c1
+
+def derivar_polinomio(P):
+    P1 = []
+    for i in range(1, len(P)):
+        P1.append(i * P[i])
+    return P1
+
+def evaluar_derivadas(P, limite=100):
+    for num in range(1, limite+1):
+        Q = P[:]
+        es_valido = True
+
+        while len(Q) > 0:
+            if evaluar_polinomio(Q, num) <= 0:
+                es_valido = False
+                break
+            Q = derivar_polinomio(Q)
+
+        if es_valido:
+            return num
+    return None
+
+
+def cotasNewton(P):
+    
+    c1 = evaluar_derivadas(P)
+    c2 = 1 / evaluar_derivadas(list(reversed(P)))
+    c4 = - evaluar_derivadas(transformar_para_cotas_negativas(P))
+    c3 = - 1 / evaluar_derivadas(list(reversed(transformar_para_cotas_negativas(P))))
+    
+    return c4, c3, c2, c1
+
+#g) Encontrar raíces reales por el método de Newtos para polinomios.
+
+#— Implemente el Método de Bairstow.
+
+
+P = [1, 0, -5, 0, 4] 
+
+print(cotasLaguerre(P))
+print(cotasNewton(P))
+
+    
