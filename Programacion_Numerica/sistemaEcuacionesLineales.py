fb745241962ce24214dc51caafe3d8bfd61bdea9
NahimMora <nahimsalta@gmail.com>
2025-10-12 22:45:30 -0300

    Gauss Jordan, falta sucesiondiagonal.

--- Programacion_Numerica/sistemaEcuacionesLineales.py
+++ Programacion_Numerica/sistemaEcuacionesLineales.py
@@ -25,20 +25,26 @@ def normaVector(V):
             max = V[i]
     return max
 
+def rotarFilas(A, i1, i2):
+    n = A.shape(1)
+    
+    for j in range (n):
+        aux = A[i1][j]
+        A[i1][j] = A[i2][j]
+        A[i2][j] = aux
+    return A
+
 def pivoteoSimple(A, k):
     if A[k][k] != 0 and k < A.shape(0):
         return A
     
-    for i in range (A.shape[0]):
-        aux = A[i][k]
-        A[i][k] = A[i+1][k]
-        A[i+1][k] = aux
+    A = rotarFilas(A, k, k+1)
     
-    return pivoteoSimple(A, k)
+    return pivoteoSimple(A, k+1)
 
 def sustitucionRegresiva(G):
-    x = [0] * n 
     n = G.shape(0)
+    x = [0] * n 
     
     for i in range (n-1, -1, -1):
         suma = 0
@@ -46,13 +52,18 @@ def sustitucionRegresiva(G):
             suma += G[i][j] * x[j]
         x[i] = (G[i][-1] - suma) / G[i][i]
 
+def sustitucionDiagonal(G):
+    n = G.shape(0)
+    x = [0] * n 
+    
+
 def gauss(A, b):
     
     n = A.shape[0]
     G = np.hstack((A, b))
         
     for k in range (1, n-1):
-        A = pivoteoSimple(A, k)
+        G = pivoteoSimple(G, k)
         
         for i in range (k+1, n):
             m = G[i, k] / G[k, k]
@@ -61,3 +72,20 @@ def gauss(A, b):
             for j in range (k+1, n+1):
                 G[i, j] = G[i, j] - m* G[k, j]
     return sustitucionRegresiva(G)
+
+def gaussJordan(A, b):
+    
+    G = np.hstack((A, b))
+    n = A.shape(0)
+    
+    for k in range (1, n):
+        G = pivoteoSimple(G, k)
+        
+        for i in range (1, n):
+            if i != k:
+                m = G[i][k]/G[k][k]
+                
+                for j in range (k, n+1):
+                    G[i][j] = G[i][j] - m* G[k][j]
+    return sustitucionDiagonal(G)
+    
